bdf5e8c7795474bb6adadb0c20febb582a898f97
valenn0101 <veroncaceresvalentin@gmail.com>
2023-02-14 23:52:32 -0300

    Aplicacion Terminada, falta test
    Se agrego una ventana modal al dar click en 'information' para mostrar las estadisticas de cada pokemon.
    
    Ademas se aplica una espera de 1seg al cambiar de pagina para evitar fallos en la carga de los pokemones.

--- src/js/main.js
+++ src/js/main.js
@@ -34,11 +34,53 @@ function showPokemon(pokemonesJSON) {
     ${types}
     </div>
     <div class="informacion">
-      <button>Information</button>
+      <button class="modal-btn">Information</button>
     </div>
   </div>
 </div>
   `;
+  const modalBtn = div.querySelector(".modal-btn");
+  const modal = document.getElementById("myModal");
+  const modalTitle = document.getElementById("modal-title");
+  const modalBody = document.querySelector(".modal-body");
+  const closeModalBtn = document.querySelector(".close");
+
+  modalBtn.addEventListener("click", () => {
+    modalTitle.textContent = pokemonesJSON.name;
+    modalBody.innerHTML = `
+      <div class="pokemon-imagen">
+        <img src="${
+          pokemonesJSON.sprites.other["official-artwork"].front_default
+        }" alt="${pokemonesJSON.name}">
+      </div>
+      <div class="pokemon-tipos">
+        ${types}
+      </div>
+      <div class="poke-stats">
+      <p>Height: ${pokemonesJSON.height / 10} m</p>
+      <p>Weight: ${pokemonesJSON.weight / 10} kg</p>
+      ${pokemonesJSON.stats
+        .map(
+          (stat) =>
+            `<p class="atribute-stats">${stat.stat.name}: ${stat.base_stat}</p>`
+        )
+        .join("")}
+    </div>
+    `;
+
+    modal.style.display = "block";
+  });
+
+  closeModalBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+
   pokemonList.append(div);
 }
 
@@ -61,35 +103,45 @@ pokemonTypes.forEach((tipo) => {
 document.querySelector("#ver-todos").addEventListener("click", () => {
   const pokemon = document.querySelectorAll(".card");
   pokemon.forEach((pokemon) => {
-  pokemon.style.display = "flex";
+    pokemon.style.display = "flex";
   });
-  });
+});
 
 let pageNumber = 0;
-let searchRange = []
+let searchRange = [];
 
-previousPage.addEventListener("click", function(){
-pageNumber--;
-if(pageNumber < 0){
-pageNumber = 0;
-}
-switchPage();
+previousPage.addEventListener("click", function () {
+  pageNumber--;
+  if (pageNumber < 0) {
+    pageNumber = 0;
+  }
+  setTimeout(switchPage, 1000);
 });
 
-nextPage.addEventListener("click", function(){
-pageNumber++;
-switchPage();
+nextPage.addEventListener("click", function () {
+  pageNumber++;
+  setTimeout(switchPage, 1000);
 });
 
-const generaciones = [  {tittle: 'First Generation', start: 1, end: 151},  {tittle: 'Second Generation', start: 152, end: 251},  {tittle: 'Third Generation', tittle: 252, end: 386},  {tittle: 'Fourth Generation', start: 387, end: 494}, {tittle: "Fifth Generation", start: 495, end: 649}, {tittle: "Sixth  Generation", start: 650, end: 721}, {tittle: "Seventh Generation", start: 722, end: 809}, {tittle: "Eight Generation", start: 810, end: 905}, {tittle: "Ninth Generation", start: 906, end: 1008}];
+const generations = [
+  { tittle: "First Generation", start: 1, end: 151 },
+  { tittle: "Second Generation", start: 152, end: 251 },
+  { tittle: "Third Generation", start: 252, end: 386 },
+  { tittle: "Fourth Generation", start: 387, end: 494 },
+  { tittle: "Fifth Generation", start: 495, end: 649 },
+  { tittle: "Sixth  Generation", start: 650, end: 721 },
+  { tittle: "Seventh Generation", start: 722, end: 809 },
+  { tittle: "Eight Generation", start: 810, end: 905 },
+  { tittle: "Ninth Generation", start: 906, end: 1008 },
+];
 
 function switchPage() {
-  const generationName = document.querySelector(".numero-de-generacion")
-  const currentGeneration = generaciones[pageNumber];
+  const generationName = document.querySelector(".numero-de-generacion");
+  const currentGeneration = generations[pageNumber];
   generationName.innerHTML = currentGeneration.tittle;
   previousPage.disabled = pageNumber === 0;
-  nextPage.disabled = pageNumber === generaciones.length - 1;
+  nextPage.disabled = pageNumber === generations.length - 1;
   searchRange = [currentGeneration.start, currentGeneration.end];
   getPokemon(searchRange);
 }
-  switchPage();
+switchPage();
