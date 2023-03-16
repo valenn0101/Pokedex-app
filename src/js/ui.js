5ef976592a48bd1cca6d748e4b73e796780b2720
valenn0101 <veroncaceresvalentin@gmail.com>
2023-03-16 13:23:17 -0300

    Cambios en el modulo UI
    Se dividio el bloque showPokemon ya que era muy extenso, ahora esta compuesto por otras 3 funciones.

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,18 +1,56 @@
-export default function showPokemon(pokemon) {
-  const pokemonList = document.querySelector('.pokemon-cards');
-  const div = document.createElement('div');
-
-  let types = pokemon.types.map(
-    (type) => `<p class="${type.type.name} type">${type.type.name}</p>`,
-  );
-  types = types.join('');
-
-  let pokemonId = pokemon.id.toString();
+function formatPokemonId(id) {
+  let pokemonId = id.toString();
   if (pokemonId.length === 1) {
     pokemonId = `00${pokemonId}`;
   } else if (pokemonId.length === 2) {
     pokemonId = `0${pokemonId}`;
   }
+  return pokemonId;
+}
+function generatePokemonTypeHTML(type) {
+  return `<p class="${type.type.name} type">${type.type.name}</p>`;
+}
+function showModal(pokemon, types) {
+  const modal = document.getElementById('myModal');
+  const modalTitle = document.getElementById('modal-title');
+  const modalBody = document.querySelector('.modal-body');
+  const closeModalBtn = document.querySelector('.close');
+
+  modalTitle.textContent = pokemon.name;
+  modalBody.innerHTML = `
+    <div class="pokemon-img">
+      <img src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}">
+    </div>
+    <div class="pokemon-types">
+      ${types}
+    </div>
+    <div class="poke-stats">
+      <p>Height: ${pokemon.height / 10} m</p>
+      <p>Weight: ${pokemon.weight / 10} kg</p>
+      ${pokemon.stats.map((stat) => `<p class="atribute-stats">${stat.stat.name}: ${stat.base_stat}</p>`).join('')}
+    </div>
+  `;
+
+  modal.style.display = 'block';
+
+  closeModalBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  });
+}
+
+export default function showPokemon(pokemon) {
+  const pokemonList = document.querySelector('.pokemon-cards');
+  const div = document.createElement('div');
+
+  const types = pokemon.types.map(generatePokemonTypeHTML).join('');
+
+  const pokemonId = formatPokemonId(pokemon.id);
 
   div.classList.add('card');
   div.innerHTML = `
@@ -36,42 +74,8 @@ export default function showPokemon(pokemon) {
 </div>
   `;
   const modalBtn = div.querySelector('.modal-btn');
-  const modal = document.getElementById('myModal');
-  const modalTitle = document.getElementById('modal-title');
-  const modalBody = document.querySelector('.modal-body');
-  const closeModalBtn = document.querySelector('.close');
-
   modalBtn.addEventListener('click', () => {
-    modalTitle.textContent = pokemon.name;
-    modalBody.innerHTML = `
-      <div class="pokemon-img">
-        <img src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}">
-      </div>
-      <div class="pokemon-types">
-        ${types}
-      </div>
-      <div class="poke-stats">
-      <p>Height: ${pokemon.height / 10} m</p>
-      <p>Weight: ${pokemon.weight / 10} kg</p>
-      ${pokemon.stats
-    .map(
-      (stat) => `<p class="atribute-stats">${stat.stat.name}: ${stat.base_stat}</p>`,
-    )
-    .join('')}
-    </div>
-    `;
-
-    modal.style.display = 'block';
-  });
-
-  closeModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
-
-  window.addEventListener('click', (event) => {
-    if (event.target == modal) {
-      modal.style.display = 'none';
-    }
+    showModal(pokemon, types);
   });
 
   pokemonList.append(div);
